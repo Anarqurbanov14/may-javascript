42e7b3eb7d4eb2ef01d7041133e7292baff10351
Anar <anar.k14@mail.ru>
2024-05-13 20:28:53 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+"use strict";
+
+// anar = "name";
+//console.log(anar);
+
+//const answer = prompt("tebe 18 let", "18");
+
+//console.log(answer);
+
+let nomer = 10;
+++nomer;
+console.log(nomer);
